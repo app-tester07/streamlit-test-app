67c567cd942c7c35c26d3fb1bf3240808b4dd45e
app-tester07 <slushiepengwin@gmail.com>
2026-09-25 23:27:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ load_css("styles.css")
 
 # st.markdown('', unsafe_allow_html=True)
 
-st.markdown('<h1 id="main-heading">TEST APP</h1>', unsafe_allow_html=True)
+st.markdown('<h1 style="color: blue !important;">TEST APP</h1>', unsafe_allow_html=True)
 st.markdown('<p class="subtitle-text">Testing Application</p>', unsafe_allow_html=True)
