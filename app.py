24401dba3fbd21c1943f628329e94dc656a6e53a
app-tester07 <slushiepengwin@gmail.com>
2026-09-25 23:01:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-st.title("TEST APP")
-st.write("Testing Application")
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+load_css("styles.css")
+
+# st.markdown('', unsafe_allow_html=True)
+
+st.markdown('<h1 id="main-heading">TEST APP</h1>', unsafe_allow_html=True)
+st.markdown('<p class="subtitle-text">Testing Application</p>', unsafe_allow_html=True)
